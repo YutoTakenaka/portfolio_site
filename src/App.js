9f1670e32f650e11b493e611a8c969cdcca56281
YutoTakenaka <yuto.basuke0508@icloud.com>
2022-11-18 09:22:49 +0900

    feature: SkillsコンポーネントとContactコンポーネントの実装

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Contact } from "./components/Contact";
 import { Header } from "./components/Header";
+import { Skills } from "./components/Skills";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Skills />
+      <Contact />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,12 @@
+export const Skills = () => {
+  return (
+    <div id="skills">
+      <div className="container">
+        <div className="heading">
+          <p>Skills</p>
+        </div>
+        <div className="skills-container"></div>
+      </div>
+    </div>
+  );
+};
