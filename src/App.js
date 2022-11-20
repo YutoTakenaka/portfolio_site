af3fc02bef2f1c1a8dabf09e0310520ac5ef4f72
YutoTakenaka <yuto.basuke0508@icloud.com>
2022-11-20 17:56:04 +0900

    fix: スタイルのファイル分け

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import "./App.css";
+import "./styles/App.css";
 import { Contact } from "./components/Contact";
 import { Header } from "./components/Header";
 import { Skills } from "./components/Skills";
+import { Top } from "./components/Top";
 
 function App() {
   return (
     <>
-      <Header />
+      <Top />
       <Skills />
       <Contact />
     </>
---
+++ src/components/Top.jsx
@@ -0,0 +1,45 @@
+import "../styles/Top.css";
+import CoverImage from "../images/cover.png";
+import ProfileImage from "../images/profile.jpg";
+import { FaTwitter, FaGithub, FaInstagram } from "react-icons/fa";
+
+export const Top = () => {
+  return (
+    <div
+      className="main-cover"
+      style={{ backgroundImage: `url(${CoverImage})` }}
+    >
+      {/* overlayはカバー画像の上に透過して表示される背景要素です */}
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            <div
+              className="profile-thumb"
+              style={{ backgroundImage: `url(${ProfileImage})` }}
+            ></div>
+            <p className="title-text text-lg">Yuto Takenaka</p>
+            <p className="title-text text-sm">Web Developer</p>
+            <ul className="social-icons">
+              <li className="icon-link">
+                <a href="https://twitter.com/eutu_ty">
+                  <FaTwitter color="white" size="2rem" />
+                </a>
+              </li>
+              <li className="icon-link">
+                <a href="https://www.instagram.com/eutu_ty/">
+                  <FaInstagram color="white" size="2rem" />
+                </a>
+              </li>
+              <li className="icon-link">
+                <a href="https://github.com/YutoTakenaka">
+                  <FaGithub color="white" size="2rem" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
