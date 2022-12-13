70d7f408b2eeb96e8f00e3b4b95e922ef589d969
YutoTakenaka <yuto.basuke0508@icloud.com>
2022-12-14 08:33:54 +0900

    SVGファイルの追加とTOPコンポーネントの修正

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,35 +1,44 @@
 import "../styles/About.css";
 import Image from "../images/programmer.svg";
+import CoverImage from "../images/font.svg";
 
 export const About = () => {
   return (
-    <div className="about">
-      <p className="title">About</p>
-      <div className="message">
-        <p className="message-title">「ITの力で人々の人生に追い風を」</p>
-        <p className="message-text">このような想いを掲げて仕事をしています。</p>
-      </div>
-      <div className="profile-container">
-        <div className="left-profile">
-          <div
-            className="profile-image"
-            style={{ backgroundImage: `url(${Image})` }}
-          ></div>
-        </div>
-        <div className="right-profile">
-          <p className="profile">Name: 竹中 優斗(たけなか ゆうと)</p>
-          <p className="profile">D.O.B: 1999年5月8日</p>
-          <p className="profile">From: 青森県八戸市</p>
-          <p className="profile">
-            Favorite: バスケットボール/ラーメン巡り/一眼レフカメラ
+    <>
+      <div className="about">
+        <p className="title">About</p>
+        <div className="message">
+          <p className="message-title">「ITの力で人々の人生に追い風を」</p>
+          <p className="message-text">
+            このような想いを掲げて仕事をしています。
           </p>
-          <hr />
-          <p className="profile-text">
-            千葉大学法政経学部法政経学科を卒業し、新卒でIT企業に就職しました。現在はReact,TypeScriptなどを用いたモダンなフロントエンド開発をしています。私に関わったことで、私が開発に携わったアプリを使うことで、誰かの人生にいい影響を与えられると嬉しいです。まだまだ駆け出しのエンジニアですが、ユーザーに寄り添った実装を心がけて日々開発しています。
-          </p>
-          <p className="profile-text"></p>
+        </div>
+        <div className="profile-container">
+          <div className="left-profile">
+            <div
+              className="profile-image"
+              style={{ backgroundImage: `url(${Image})` }}
+            ></div>
+          </div>
+          <div className="right-profile">
+            <p className="profile">Name: 竹中 優斗(たけなか ゆうと)</p>
+            <p className="profile">D.O.B: 1999年5月8日</p>
+            <p className="profile">From: 青森県八戸市</p>
+            <p className="profile">
+              Favorite: バスケットボール/ラーメン巡り/一眼レフカメラ
+            </p>
+            <hr />
+            <p className="profile-text">
+              千葉大学法政経学部法政経学科を卒業し、新卒でIT企業に就職しました。現在はReact,TypeScriptなどを用いたモダンなフロントエンド開発をしています。私に関わったことで、私が開発に携わったアプリを使うことで、誰かの人生にいい影響を与えられると嬉しいです。まだまだ駆け出しのエンジニアですが、ユーザーに寄り添った実装を心がけて日々開発しています。
+            </p>
+            <p className="profile-text"></p>
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        className="cover"
+        style={{ backgroundImage: `url(${CoverImage})` }}
+      />
+    </>
   );
 };
--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,53 +1,60 @@
-import CoverImage from "../images/sea.jpg";
+import CoverImage from "../images/font.svg";
+import Image from "../images/pair.svg";
 import "../styles/Favorite.css";
 
 export const Favorite = () => {
   return (
-    <div className="favorite" style={{ backgroundImage: `url(${CoverImage})` }}>
-      <div className="overlay"></div>
-      <div className="favorite-container">
-        <div className="favorite-header">
-          <p className="favorite-title">Favorite</p>
-          <p className="favorite-message">
-            私の心を動かしてくれるものを紹介します。
-          </p>
-        </div>
-        <div className="favorite-contents">
-          {/* 1 */}
-          <div className="contents">
-            <div className="contents-header">
-              <p className="contents-title">バスケットボール</p>
-              <p className="contents-title-en">Basketball</p>
-            </div>
-            <div className="contents-body">
-              <p>aaa</p>
-              <p>aaa</p>
-            </div>
+    <>
+      <div className="favorite" style={{ backgroundImage: `url(${Image})` }}>
+        <div className="overlay"></div>
+        <div className="favorite-container">
+          <div className="favorite-header">
+            <p className="favorite-title">Favorite</p>
+            <p className="favorite-message">
+              私の心を動かしてくれるものを紹介します。
+            </p>
           </div>
-          {/* 2 */}
-          <div className="contents">
-            <div className="contents-header">
-              <p className="contents-title">ラーメンパトロール</p>
-              <p className="contents-title-en">Ramen Patrol</p>
+          <div className="favorite-contents">
+            {/* 1 */}
+            <div className="contents">
+              <div className="contents-header">
+                <p className="contents-title">バスケットボール</p>
+                <p className="contents-title-en">Basketball</p>
+              </div>
+              <div className="contents-body">
+                <p>aaa</p>
+                <p>aaa</p>
+              </div>
             </div>
-            <div className="contents-body">
-              <p>aaa</p>
-              <p>aaa</p>
+            {/* 2 */}
+            <div className="contents">
+              <div className="contents-header">
+                <p className="contents-title">ラーメンパトロール</p>
+                <p className="contents-title-en">Ramen Patrol</p>
+              </div>
+              <div className="contents-body">
+                <p>aaa</p>
+                <p>aaa</p>
+              </div>
             </div>
-          </div>
-          {/* 3 */}
-          <div className="contents">
-            <div className="contents-header">
-              <p className="contents-title">一眼レフカメラ</p>
-              <p className="contents-title-en">SLR Camera</p>
-            </div>
-            <div className="contents-body">
-              <p>aaa</p>
-              <p>aaa</p>
+            {/* 3 */}
+            <div className="contents">
+              <div className="contents-header">
+                <p className="contents-title">一眼レフカメラ</p>
+                <p className="contents-title-en">SLR Camera</p>
+              </div>
+              <div className="contents-body">
+                <p>aaa</p>
+                <p>aaa</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      {/* <div
+        className="cover"
+        style={{ backgroundImage: `url(${CoverImage})` }}
+      /> */}
+    </>
   );
 };
