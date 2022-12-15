13cd17549560e20ba7d3f506250a6a5ea8ffacc5
YutoTakenaka <yuto.basuke0508@icloud.com>
2022-12-15 22:56:53 +0900

    add:Skillsコンポーネントの実装

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,69 @@
 import "../styles/Skills.css";
-import Circle from "react-circle";
+import Cloud from "../images/cloud.svg";
+import { Radar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+  CategoryScale,
+} from "chart.js";
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  CategoryScale,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
 
 export const Skills = () => {
-  const LanguageList = [
-    { language: "React", percent: 45 },
-    { language: "TypeScript", percent: 45 },
-    { language: "JavaScript", percent: 45 },
-    { language: "Python", percent: 20 },
-    { language: "PHP", percent: 40 },
-    { language: "Ruby", percent: 60 },
+  const dataLabels = [
+    "React",
+    "TypeScript",
+    "JavaScript",
+    "Python",
+    "PHP",
+    "Ruby",
   ];
+  const skillData = {
+    labels: dataLabels,
+    datasets: [
+      {
+        label: "skills(%)",
+        data: [70, 70, 60, 30, 40, 60],
+        backgroundColor: "rgba(42,61,224,0.3)",
+        borderColor: "rgba(42,61,224,1)",
+        borderWidth: 3,
+      },
+    ],
+  };
+  const options = {
+    elements: {
+      line: { borderWidth: 3 },
+    },
+    plugin: {
+      // legend: { display: false },
+      // datalabels: { display: false },
+    },
+    scales: {
+      r: {
+        min: 0,
+        max: 100,
+        ticks: {
+          stepSize: 20,
+        },
+        angleLines: {
+          display: false,
+        },
+      },
+    },
+  };
 
   return (
     <div id="skills">
@@ -18,18 +72,12 @@ export const Skills = () => {
           <p>Skills</p>
         </div>
         <div className="skills-container">
-          {/* <AiOutlineLoading3Quarters className="loading" /> */}
-          {LanguageList.map((item, index) => (
-            <div className="skill-item" key={index}>
-              <p className="description strong">{item.language}</p>
-              <Circle
-                animate
-                progress={item.percent}
-                progressColor="#047ff3"
-                size="128px"
-              />
-            </div>
-          ))}
+          <div className="">
+            <img className="react-image" src={Cloud} alt="" />
+          </div>
+          <div className="chart">
+            <Radar data={skillData} options={options} />
+          </div>
         </div>
       </div>
     </div>
