5a88b88072465a497f1e9b920626be01e591844b
YutoTakenaka <yuto.basuke0508@icloud.com>
2023-04-02 22:36:59 +0900

    add:Favoriteページの写真を追加

--- src/components/pages/FavoritePage.jsx
+++ src/components/pages/FavoritePage.jsx
@@ -1,9 +1,17 @@
 import { Favorite } from "../molecules/Favorite";
+import { Slick } from "../molecules/Slick";
 
 export const FavoritePage = () => {
   return (
     <>
       <Favorite />
+      <div className="photo-space">
+        <div className="photo-header">
+          <p className="photo-title">Photograph</p>
+          <p className="photo-message">-心を揺さぶられた写真-</p>
+        </div>
+        <Slick />
+      </div>
     </>
   );
 };
